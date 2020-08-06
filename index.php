3ac0b4ec39862f4a8b17697a3140712705980e72
sendyjoan <sendyjoan5@gmail.com>
2020-08-06 21:37:39 +0700

    mengubah file index menambahkan file frog.php menambahkan file ape.php

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
-require ('animal.php');
+require('animal.php');
+require('frog.php');
+require('ape.php');
 
 $sheep = new Animal("shaun");
     echo $sheep->name;
+
+
+echo "<br>";
+$kodok = new Frog("buduk");
+$kodok->jump() ; // "hop hop"
+echo "<br>";
+echo $kodok->name;
+echo "<br>";
+$sungokong = new Ape("kera sakti");
+$sungokong->yell() // "Auooo"
+
 ?>
